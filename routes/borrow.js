5f1a9ce8cbda903f31a3604900ec5caf5c20951f
Eumatthew Cobalida <Letmego.MC@gmail.com>
2022-10-18 21:05:25 +0100

    added borrow book functionality

---
+++ routes/borrow.js
@@ -0,0 +1,94 @@
+var express = require("express");
+var router = express.Router();
+var Borrowers = require("../schemas/borrow.schema");
+var Books = require("../schemas/book.schema");
+
+router.get("/", async function (req, res, next) {
+  const data = await Borrowers.find({})
+    .populate({ path: "borrower", select: "username" })
+    .populate({ path: "book", select: "title" });
+
+  return res.send(data);
+});
+
+router.post("/book", async function (req, res) {
+  if (!req.body.userId) {
+    res.send({ message: "User ID is required" });
+  }
+
+  if (!req.body.bookId) {
+    res.send({ message: "Book ID is required" });
+  }
+
+  const data = await Books.findOne({ _id: req.body.bookId });
+  if (data) {
+    const quantity = data.toObject().quantity;
+
+    if (!quantity) {
+      return res.send({ message: "Current book is out of stock" });
+    } else {
+      Books.updateOne(
+        { _id: req.body.bookId },
+        {
+          quantity: quantity - 1,
+        },
+        function (err) {
+          if (err) {
+            return res.send({
+              message: "Error upon borrowing book. Please try again later.",
+            });
+          }
+
+          const userId = req.body.userId;
+          const bookId = req.body.bookId;
+
+          Borrowers.findOne({
+            borrower: userId,
+            book: bookId,
+          })
+            .lean()
+            .exec(function (err, doc) {
+              if (doc) {
+                Borrowers.updateOne(
+                  {
+                    _id: doc._id,
+                  },
+                  {
+                    quantity: doc.quantity + 1,
+                  },
+                  function (err) {
+                    if (err) {
+                      return res.send({
+                        message:
+                          "Error borrowing book. Please try again later.",
+                      });
+                    } else {
+                      return res.send({
+                        message: "Successfully borrowed book",
+                      });
+                    }
+                  }
+                );
+              } else {
+                Borrowers.create({
+                  borrower: req.body.userId,
+                  book: req.body.bookId,
+                  quantity: borrowedBooks,
+                })
+                  .then((response) => {
+                    return res.send({ message: "Successfully borrowed book" });
+                  })
+                  .catch((ex) => {
+                    return res.send({
+                      message: "Error borrowing book. Please try again later.",
+                    });
+                  });
+              }
+            });
+        }
+      );
+    }
+  }
+});
+
+module.exports = router;
---
+++ schemas/borrow.schema.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    borrower: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+    book: { type: mongoose.Schema.Types.ObjectId, ref: "Book" },
+    quantity: "number",
+  },
+  {
+    timestamps: true,
+  }
+);
+const Borrow = mongoose.model("Borrow", schema);
+
+module.exports = Borrow;
